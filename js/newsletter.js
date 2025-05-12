518762f49e1ad132e61d6250b64a7be74790b3df
Augustin <augustin.jaeger-seminel@oclock.school>
2025-05-12 11:34:42 +0200

    feat: now newsletter is hidden by default and can be opened and closed via DOM manipulation

---
+++ js/newsletter.js
@@ -0,0 +1,19 @@
+// Récupération des éléments du DOM
+const newsletter = document.getElementById("newsletter");
+const openNewsletter = document.getElementById("newsletter__open");
+const closeNewsletter = document.getElementById("newsletter__close");
+
+// Ajout d'un écouteur d'événement pour le bouton d'ouverture de la newsletter
+openNewsletter.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("click open");
+  newsletter.classList.toggle("newsletter--hidden");
+});
+
+// Ajout d'un écouteur d'événement pour le bouton de fermeture de la newsletter
+closeNewsletter.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("click close");
+  newsletter.classList.add("newsletter--hidden");
+});
+
