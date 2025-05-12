0f2fc3d450cb7e2b6163d6a530724264969ba6ec
Augustin <augustin.jaeger-seminel@oclock.school>
2025-05-12 12:03:11 +0200

    feat: now checking if email input in Newsletter form is part of a forbidden domains list, and displaying an error if it is

--- js/newsletter.js
+++ js/newsletter.js
@@ -23,3 +23,41 @@ closeNewsletter.addEventListener("click", (e) => {
   newsletter.classList.add("newsletter--hidden");
 });
 
+// Ajout d'une vérification des emails saisis pour refuser une liste de domaines correspondant à des emails jetables
+
+// Liste des domaines email interdits, fournie par le cours
+const forbiddenDomains = [
+    '@yopmail.com',
+    '@yopmail.fr',
+    '@yopmail.net',
+    '@cool.fr.nf',
+    '@jetable.fr.nf',
+    '@courriel.fr.nf',
+    '@moncourrier.fr.nf',
+    '@monemail.fr.nf',
+    '@monmail.fr.nf',
+    '@hide.biz.st',
+    '@mymail.infos.st',
+  ]
+
+  // Identification des éléments du DOM nécessaires
+const emailInput = document.getElementById("subscriber-email");
+const submitButton = document.getElementById("newsletter__submit");
+const errorMessage = document.getElementById("newsletter__error");
+
+// Au click sur le bouton de soumission du formulaire, on déclenche une vérification du domaine de l'email saisi, comparé à la liste fournie et on déclenche le cas échéant une alerte
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const email = emailInput.value;
+  const domain = email.substring(email.indexOf("@"));
+
+  if (forbiddenDomains.includes(domain)) {
+    errorMessage.textContent = "Utiliser un email jetable n'est pas autorisé pour vous inscrire à notre newsletter.";
+    errorMessage.style.display = "block";
+  } else {
+    errorMessage.style.display = "none";
+    console.log(`Email saisi valide : ${email}`);
+  }
+});
+
+
