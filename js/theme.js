17361dcea1ef86040c06c5eaf950d2a14301c848
Augustin <augustin.jaeger-seminel@oclock.school>
2025-05-13 18:17:56 +0200

    fix: fixed dark theme with proper id selector, addind a missing comment and added a container to wait for DOM to be loaded

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,11 @@
+// J'attends que le DOM soit chargé avant de déclencher mes scripts
+
+document.addEventListener("DOMContentLoaded", () => {
 // Je récupère les éléments du DOM pour la gestion du thème
 const greenTheme = document.getElementById("theme-green");
 const redTheme = document.getElementById("theme-red");
 const blueTheme = document.getElementById("theme-blue");
-const darkTheme = document.getElementById("theme-dark")
+const darkTheme = document.getElementById("theme-switch")
 
 
 // Gestion du thème par couleur, avec écouteur d'événement puis déclencher le thème via les class déjà prévues dans le html et le style dans le css
@@ -25,6 +28,9 @@ blueTheme.addEventListener("click", () => {
   document.body.classList.add("theme-blue");
 });
 
+// Thème sombre
 darkTheme.addEventListener("click", () => {
   document.body.classList.toggle("theme-dark");
 });
+
+})
