18a459405c0bbfd29bf02d4fc92148830b69fd37
Augustin <augustin.jaeger-seminel@oclock.school>
2025-05-13 17:33:07 +0200

    feat: now we can change website theme with buttons in header (Green, red, blue and dark)

---
+++ js/theme.js
@@ -0,0 +1,30 @@
+// Je récupère les éléments du DOM pour la gestion du thème
+const greenTheme = document.getElementById("theme-green");
+const redTheme = document.getElementById("theme-red");
+const blueTheme = document.getElementById("theme-blue");
+const darkTheme = document.getElementById("theme-dark")
+
+
+// Gestion du thème par couleur, avec écouteur d'événement puis déclencher le thème via les class déjà prévues dans le html et le style dans le css
+
+// Thème vert
+greenTheme.addEventListener("click", () => {
+  document.body.classList.remove("theme-red", "theme-blue");
+  document.body.classList.add("theme-green");
+});
+
+// Thème rouge
+redTheme.addEventListener("click", () => {
+  document.body.classList.remove("theme-green", "theme-blue");
+  document.body.classList.add("theme-red");
+});
+
+// Thème bleu
+blueTheme.addEventListener("click", () => {
+  document.body.classList.remove("theme-green", "theme-red");
+  document.body.classList.add("theme-blue");
+});
+
+darkTheme.addEventListener("click", () => {
+  document.body.classList.toggle("theme-dark");
+});
